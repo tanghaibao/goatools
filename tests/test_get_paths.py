7179e6ca99b18adfa2a79bf146b090cc518c45d3
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2015-04-01 15:04:52 -0400

    Added new GOTerm data member, depth, which tracks the depth of the longest path back to root.

--- tests/test_get_paths.py
+++ tests/test_get_paths.py
@@ -1,6 +1,17 @@
 
-from goatools.obo_parser import GODag
+
 import sys
+
+# Test local version of goatools
+sys.path.insert(0, '..')
+
+from goatools.obo_parser import GODag
+
+def prt_paths(paths, PRT=sys.stdout):
+  for path in paths:
+    PRT.write('\n')
+    for GO in path:
+      PRT.write('{}\n'.format(GO))
 
 def chk_results(actual_paths, expected_paths):
   for actual_path in actual_paths:
@@ -10,6 +21,7 @@ def chk_results(actual_paths, expected_paths):
       raise Exception('ACTUAL {} NOT FOUND IN EXPECTED RESULTS\n'.format(actual))
 
 def test_paths_to_top():
+  #dag = GODag("./tests/data/mini_obo.obo")  
   dag = GODag("./data/mini_obo.obo")  
   expected_paths = [
     ['GO:0000001', 'GO:0000002', 'GO:0000005', 'GO:0000010'],
@@ -17,7 +29,7 @@ def test_paths_to_top():
     ['GO:0000001', 'GO:0000003', 'GO:0000006', 'GO:0000008', 'GO:0000010'] ]
   actual_paths = dag.paths_to_top("GO:0000010")
   chk_results(actual_paths, expected_paths)
-      
+  prt_paths(actual_paths)
 
 if __name__ == '__main__':
   test_paths_to_top()
