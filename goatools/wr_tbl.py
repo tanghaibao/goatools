0e92ac8a045607dfa55e855088f65ed847e1a294
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2016-02-06 14:44:15 -0500

    Added sort_by kwarg to xlsx writer. Made file docstring more descriptive.

--- goatools/wr_tbl.py
+++ goatools/wr_tbl.py
@@ -1,21 +1,24 @@
 """Contains generic table-writing functions. Data is expected to be a list of namedtuples.
 
        kwargs (kws):
-           'hdrs' : A list of headers to use when printing the table.
-               default: nt_fields is used as the table headers.
+           'hdrs' : A list of column headers to use when printing the table.
+               default: The fields in the data's namedtuple is used as the column headers.
            'print_names': Used when the user wants to print a subset of the nt fields.
-               This may occur if a field is to be used in either sort_by or prt_if, 
+               This may occur if a field is to be used in either sort_by or prt_if,
                but not to be printed.
-           'sort_by' : User-customizable sort when printing
-               sortby is a lambda function with the GO namedtuple as input.
-               It is used in the sorted function. For example:
-                   data_nts = sorted(data_nts, key=kws['sort_by'])
-           'prt_if': Only keep a line if user-specfied test returns True.
+           'sort_by' : User-customizable sort when printing.
+               sortby is a lambda function with the data item's namedtuple as input.
+               It is the 'key' used in the sorted function.
+           'prt_if': Only print a line if user-specfied test returns True.
+               prt_if is a lambda function with the data item's namedtuple as input.
            'sep': Separator used when printing the tab-separated table format.
                default: sep = '\t'
            'fld2col_widths: A dictionary of column widths used when writing xlsx files.
            'fld2fmt': Used in tsv files and xlsx files for formatting specific fields
 """
+
+__copyright__ = "Copyright (C) 2016, DV Klopfenstein, H Tang, All rights reserved."
+__author__ = "DV Klopfenstein"
 
 import re
 import sys
@@ -23,13 +26,13 @@ import sys
 def prt_txt(prt, data_nts, prtfmt, nt_fields, **kws):
     """Print list of namedtuples into a table using prtfmt."""
     _chk_flds_fmt(nt_fields, prtfmt)
-    _prt_txt_hdr(prt, nt_fields, prtfmt, **kws)
+    _prt_txt_hdr(prt, prtfmt)
     if 'sort_by' in kws:
         data_nts = sorted(data_nts, key=kws['sort_by'])
     prt_if = kws['prt_if'] if 'prt_if' in kws else None
-    for nt in data_nts:
-        if prt_if is None or prt_if(nt):
-            prt.write(prtfmt.format(**nt._asdict()))
+    for data_nt in data_nts:
+        if prt_if is None or prt_if(data_nt):
+            prt.write(prtfmt.format(**data_nt._asdict()))
 
 def wr_xlsx(fout_xlsx, xlsx_data, **kws):
     """Write a spreadsheet into a xlsx file."""
@@ -47,22 +50,29 @@ def wr_xlsx(fout_xlsx, xlsx_data, **kws):
         hdrs = flds if 'hdrs' not in kws else kws['hdrs']
         for col_idx, hdr in enumerate(hdrs):
             worksheet.write(0, col_idx, hdr, fmt_hdr)
+        row_data_start = 1
         # Print data
-        items = 0
-        fld2fmt = None if 'fld2fmt' not in kws else kws['fld2fmt']
-        prt_if = kws['prt_if'] if 'prt_if' in kws else None
-        for row_idx, nt in enumerate(xlsx_data, 1):
-            if prt_if is None or prt_if(nt):
-                _wrxlsxrow(worksheet, row_idx, nt, fmt_txt, fld2fmt)
-                items += 1
+        row_idx = _wrxlsxdata(xlsx_data, row_data_start, worksheet, fmt_txt, **kws)
         workbook.close()
-        sys.stdout.write("  {:>5} items WROTE: {}\n".format(items, fout_xlsx))
+        sys.stdout.write("  {:>5} items WROTE: {}\n".format(row_idx-row_data_start, fout_xlsx))
     else:
         sys.stdout.write("      0 items. NOT WRITING {}\n".format(fout_xlsx))
 
-def _wrxlsxrow(worksheet, row_idx, nt, fmt_txt, fld2fmt):
+def _wrxlsxdata(xlsx_data, row_idx, worksheet, fmt_txt, **kws):
+    """Write data into xlsx worksheet."""
+    fld2fmt = None if 'fld2fmt' not in kws else kws['fld2fmt']
+    prt_if = kws['prt_if'] if 'prt_if' in kws else None
+    if 'sort_by' in kws:
+        xlsx_data = sorted(xlsx_data, key=kws['sort_by'])
+    for data_nt in xlsx_data:
+        if prt_if is None or prt_if(data_nt):
+            _wrxlsxrow(worksheet, row_idx, data_nt, fmt_txt, fld2fmt)
+            row_idx += 1
+    return row_idx
+
+def _wrxlsxrow(worksheet, row_idx, data_nt, fmt_txt, fld2fmt):
     """Print an xlsx row by printing each column in order."""
-    row_fld_vals = nt._asdict().items()
+    row_fld_vals = data_nt._asdict().items()
     for col_idx, (fld, val) in enumerate(row_fld_vals):
         # Optional user-formatting of specific fields, eg, pval: "{:8.2e}"
         if fld2fmt is not None and fld in fld2fmt:
@@ -131,20 +141,13 @@ def _chk_flds_fmt(nt_fields, prtfmt):
         M=" ".join(missing_data), PF=prtfmt))
 
 def get_fmtflds(prtfmt):
-    """Return the feildnames in the formatter text."""
-    # prtfmt can look like: "{NS} {study_cnt:2} {fdr_bh:5.3e} L{level:02} D{depth:02} {GO} {name}\n"
+    """Return the fieldnames in the formatter text."""
+    # Example prtfmt: "{NS} {study_cnt:2} {fdr_bh:5.3e} L{level:02} D{depth:02} {GO} {name}\n"
     return [f.split(':')[0] for f in re.findall(r'{(\S+)}', prtfmt)]
 
-def _prt_txt_hdr(prt, nt_fields, prtfmt, **kws):
+def _prt_txt_hdr(prt, prtfmt):
     """Print header for text report."""
     tblhdrs = get_fmtflds(prtfmt)
     prt.write("{}\n".format(" ".join(tblhdrs)))
 
-def _get_tblhdrs(fields, kws):
-    """Given namedtuple fields and headers, return table headers."""
-    if 'hdrs' not in kws:
-        return fields
-    else:
-        assert len(nt_fields) == len(tblhdrs)
-        return kws['hdrs']
-
+# Copyright (C) 2016, DV Klopfenstein, H Tang. All rights reserved.
