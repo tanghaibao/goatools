28937b4095f9086518415feb736ae6073c8dd9f8
Fidel Ramirez <ramirez@ie-freiburg.mpg.de>
2016-02-11 09:53:27 +0100

    futher pep8 changes.

--- tests/test_get_paths.py
+++ tests/test_get_paths.py
@@ -5,7 +5,7 @@ from goatools.obo_parser import GODag
 ROOT = os.path.dirname(os.path.abspath(__file__)) + "/data/"
 
 
-def prt_paths(paths, PRT=sys.stdout):
+def print_paths(paths, PRT=sys.stdout):
     for path in paths:
         PRT.write('\n')
         for GO in path:
@@ -22,11 +22,9 @@ def chk_results(actual_paths, expected_paths):
 
 def test_paths_to_top():
     dag = GODag(ROOT + "mini_obo.obo")
-    expected_paths = [
-    ['GO:0000001', 'GO:0000002', 'GO:0000005', 'GO:0000010'],
-    ['GO:0000001', 'GO:0000003', 'GO:0000005', 'GO:0000010'],
-    ['GO:0000001', 'GO:0000003', 'GO:0000006', 'GO:0000008', 'GO:0000010'] ]
+    expected_paths = [['GO:0000001', 'GO:0000002', 'GO:0000005', 'GO:0000010'],
+                      ['GO:0000001', 'GO:0000003', 'GO:0000005', 'GO:0000010'],
+                      ['GO:0000001', 'GO:0000003', 'GO:0000006', 'GO:0000008', 'GO:0000010']]
     actual_paths = dag.paths_to_top("GO:0000010")
     chk_results(actual_paths, expected_paths)
-    prt_paths(actual_paths)
-
+    print_paths(actual_paths)
