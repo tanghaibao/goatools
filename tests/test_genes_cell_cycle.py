dad3216e2f20d5eecbc9a432249025f74de6442a
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2016-02-16 06:23:15 -0500

    pylint

--- tests/test_genes_cell_cycle.py
+++ tests/test_genes_cell_cycle.py
@@ -1,3 +1,4 @@
+"""Find human genes related to cell cycle."""
 
 import sys
 import os
@@ -8,30 +9,26 @@ from goatools.associations import get_assoc_ncbi_taxids
 from genes_NCBI_hsa_All import GeneID2nt
 from goatools.wr_tbl import prt_txt
 
-"""Find human genes related to cell cycle."""
-
-ROOT = os.path.dirname(os.path.abspath(__file__)) + "/data/"
-
 __copyright__ = "Copyright (C) 2010-2016, H Tang et al., All rights reserved."
 __author__ = "DV Klopfenstein"
 
-
-def test_cell_cycle(taxid, log=sys.stdout):
+def test_cell_cycle(log=sys.stdout):
     """Test GOEA with local multipletest correction methods."""
+    taxid = 9606 # Human annotations
     # Download ontologies and annotations, if necessary
     fin_go_obo = "go-basic.obo"
     if not os.path.exists(fin_go_obo):
-      os.system("wget http://geneontology.org/ontology/go-basic.obo")
+        os.system("wget http://geneontology.org/ontology/go-basic.obo")
     taxid2asscs = get_assoc_ncbi_taxids([taxid])
     # Initialize GO-search helper object
     srch = GoSearch(fin_go_obo, go2items=taxid2asscs[taxid]['GO2GeneIDs'])
     # Compile search pattern for 'cell cycle'
     cell_cycle = re.compile(r'cell cycle', flags=re.IGNORECASE)
-    # Find ALL GOs that have "cell cycle". Store results in file.
+    # Find ALL GOs that have 'cell cycle'. Store results in file.
     fout_allgos = "cell_cycle_gos.log"
     with open(fout_allgos, "w") as prt:
-       gos = srch.get_matching_gos(cell_cycle, prt)
-       log.write("    WROTE {N:>5} gos:   {FOUT}\n".format(FOUT=fout_allgos, N=len(gos)))
+        gos = srch.get_matching_gos(cell_cycle, prt)
+        log.write("    WROTE {N:>5} gos:   {FOUT}\n".format(FOUT=fout_allgos, N=len(gos)))
     # Researcher carefully reviews GO results and finds GO:0005764(lysosome)
     # in the results when it should not be because the match was found:
     #     cell cycle-independent
@@ -51,6 +48,6 @@ def test_cell_cycle(taxid, log=sys.stdout):
 
 
 if __name__ == '__main__':
-    test_cell_cycle(taxid=9606) # human annotations
+    test_cell_cycle()
 
 # Copyright (C) 2010-2016, H Tang et al., All rights reserved.
