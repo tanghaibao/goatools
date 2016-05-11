18459690ed28d84a322b89ec8d402ee2df0ca759
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2016-05-11 16:44:32 -0400

    Get list of GOTerms just once.

--- tests/test_go_print.py
+++ tests/test_go_print.py
@@ -12,7 +12,7 @@ def test_go_print(prt=sys.stdout):
     prt.write("\n{OBJ}\n\n".format(OBJ=reader))
     go_terms = list(reader)
     prt.write("First GO Record: {REC}\n".format(REC=go_terms[0]))
-    for idx, go_rec in enumerate([rec for rec in reader]):
+    for idx, go_rec in enumerate(go_terms):
         prt.write("{I:>7,} {RECORD}\n".format(I=idx, RECORD=go_rec))
 
 if __name__ == '__main__':
