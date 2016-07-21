3be7e9d1434bdf2f0c8d1065286b2269882dad0e
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2016-07-21 17:00:16 -0400

    Compare list to list

--- tests/test_zip_nt_lists.py
+++ tests/test_zip_nt_lists.py
@@ -21,7 +21,7 @@ def test_zip_nt_lists():
     hdrs = ['idx', 'NS', 'level', 'depth', 'GO',
             'study_count', 'study_n', 'pop_count', 'pop_n', 'p_fdr_bh', 'name']
     lst_sub = zip_nt_lists([lst2_nts, goea_nts], hdrs)
-    assert lst_sub[0]._fields == hdrs
+    assert list(lst_sub[0]._fields) == hdrs, "{F} {H}".format(F=lst_sub[0]._fields, H=hdrs)
 
 
 def get_goea_results(method="fdr_bh"):
