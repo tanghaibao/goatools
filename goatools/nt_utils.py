e4e3246c175c9f488fc4488211890781300a84d4
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2016-07-25 02:18:10 -0400

    pylint.

--- goatools/nt_utils.py
+++ goatools/nt_utils.py
@@ -16,30 +16,14 @@ def combine_id2nts(ids, id2nts, flds, dflt_null=""):
         combined_nt_go2nt.append(ntobj._make(vals))
     return combined_nt_go2nt
 
-def _combine_nt_vals(nts, flds, dflt_null):
-    """Take a list of go2nts, return combined values for a single namedtuple."""
-    vals = []
-    # 2a. Collect values for each namedtuple field
-    for fld in flds:
-        fld_seen = False
-        for id2nt in id2nts:
-            nt_curr = id2nt.get(item_id)
-            if nt_curr is not None: 
-                if hasattr(nt_curr, fld):
-                    vals.append(getattr(nt_curr, fld))
-                    fld_seen = True
-                    break
-        if fld_seen is False:
-            vals.append(dflt_null) # Default val if GO id or nt val is not present
-    return vals
-
 # -- combine_nt_lists ----------------------------------------------------------------
 def combine_nt_lists(lists, flds, dflt_null=""):
     """Return a new list of namedtuples by zipping "lists" of namedtuples or objects."""
     combined_nt_list = []
     # Check that all lists are the same length
     lens = [len(lst) for lst in lists]
-    assert len(set(lens)) == 1, "LIST LENGTHS MUST BE EQUAL: {Ls}".format(Ls=" ".join(str(l) for l in lens))
+    assert len(set(lens)) == 1, \
+        "LIST LENGTHS MUST BE EQUAL: {Ls}".format(Ls=" ".join(str(l) for l in lens))
     # 1. Instantiate namedtuple object
     ntobj = cx.namedtuple("Nt", " ".join(flds))
     # 2. Loop through zipped list
@@ -53,9 +37,9 @@ def _combine_nt_vals(lst0_lstn, flds, dflt_null):
     vals = []
     for fld in flds:
         fld_seen = False
-        for ntitem in lst0_lstn:
-            if hasattr(ntitem, fld):
-                vals.append(getattr(ntitem, fld))
+        for nt_curr in lst0_lstn:
+            if hasattr(nt_curr, fld):
+                vals.append(getattr(nt_curr, fld))
                 fld_seen = True
                 break
         if fld_seen is False:
