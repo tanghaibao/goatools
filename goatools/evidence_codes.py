9d11e0c872a9a6e0d5a91c181082f431f8253493
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2016-09-27 03:23:46 -0400

    Added sorting routines to return evidence codes in the same order as seen on GOC website.

--- goatools/evidence_codes.py
+++ goatools/evidence_codes.py
@@ -11,37 +11,45 @@ class EvidenceCodes(object):
 
     ntobj = cx.namedtuple("NtCode", "group name")
 
-    code2name = {
+    code2name = cx.OrderedDict([
         # Experimental Evidence codes:
-        "EXP":ntobj(group="Experimental", name="Inferred from Experiment"),
-        "IDA":ntobj(group="Experimental", name="Inferred from Direct Assay"),
-        "IPI":ntobj(group="Experimental", name="Inferred from Physical Interaction"),
-        "IMP":ntobj(group="Experimental", name="Inferred from Mutant Phenotype"),
-        "IGI":ntobj(group="Experimental", name="Inferred from Genetic Interaction"),
-        "IEP":ntobj(group="Experimental", name="Inferred from Expression Pattern"),
+        ("EXP", ntobj._make(["Experimental", "Inferred from Experiment"])),
+        ("IDA", ntobj._make(["Experimental", "Inferred from Direct Assay"])),
+        ("IPI", ntobj._make(["Experimental", "Inferred from Physical Interaction"])),
+        ("IMP", ntobj._make(["Experimental", "Inferred from Mutant Phenotype"])),
+        ("IGI", ntobj._make(["Experimental", "Inferred from Genetic Interaction"])),
+        ("IEP", ntobj._make(["Experimental", "Inferred from Expression Pattern"])),
 
-        # Computational Analysis evidence codes:
-        "ISS":ntobj(group="Computational", name="Inferred from Sequence or structural Similarity"),
-        "ISO":ntobj(group="Computational", name="Inferred from Sequence Orthology"),
-        "ISA":ntobj(group="Computational", name="Inferred from Sequence Alignment"),
-        "ISM":ntobj(group="Computational", name="Inferred from Sequence Model"),
-        "IGC":ntobj(group="Computational", name="Inferred from Genomic Context"),
-        "IBA":ntobj(group="Computational", name="Inferred from Biological aspect of Ancestor"),
-        "IBD":ntobj(group="Computational", name="Inferred from Biological aspect of Descendant"),
-        "IKR":ntobj(group="Computational", name="Inferred from Key Residues"),
-        "IRD":ntobj(group="Computational", name="Inferred from Rapid Divergence"),
-        "RCA":ntobj(group="Computational", name="Inferred from Reviewed Computational Analysis"),
+        # Computational Analysis evidence codes
+        ("ISS", ntobj._make(["Computational", "Inferred from Sequence or structural Similarity"])),
+        ("ISO", ntobj._make(["Computational", "Inferred from Sequence Orthology"])),
+        ("ISA", ntobj._make(["Computational", "Inferred from Sequence Alignment"])),
+        ("ISM", ntobj._make(["Computational", "Inferred from Sequence Model"])),
+        ("IGC", ntobj._make(["Computational", "Inferred from Genomic Context"])),
+        ("IBA", ntobj._make(["Computational", "Inferred from Biological aspect of Ancestor"])),
+        ("IBD", ntobj._make(["Computational", "Inferred from Biological aspect of Descendant"])),
+        ("IKR", ntobj._make(["Computational", "Inferred from Key Residues"])),
+        ("IRD", ntobj._make(["Computational", "Inferred from Rapid Divergence"])),
+        ("RCA", ntobj._make(["Computational", "Inferred from Reviewed Computational Analysis"])),
 
-        # Author Statement evidence codes:
-        "TAS":ntobj(group="Author", name="Traceable Author Statement"),
-        "NAS":ntobj(group="Author", name="Non-traceable Author Statement"),
+        # Author Statement evidence codes
+        ("TAS", ntobj._make(["Author", "Traceable Author Statement"])),
+        ("NAS", ntobj._make(["Author", "Non-traceable Author Statement"])),
 
-        # Curatorial Statement codes:
-        "IC":ntobj(group="Curatorial", name="Inferred by Curator"),
-        "ND":ntobj(group="Curatorial", name="No biological Data available"),
+        # Curatorial Statement codes
+        ("IC", ntobj._make(["Curatorial", "Inferred by Curator"])),
+        ("ND", ntobj._make(["Curatorial", "No biological Data available"])),
 
-        # Automatically-Assigned evidence code:
-        "IEA":ntobj(group="Automatic", name="Inferred from Electronic Annotation")}
+        # Automatically-Assigned evidence code
+        ("IEA", ntobj._make(["Automatic", "Inferred from Electronic Annotation"]))])
+
+    def __init__(self):
+        self.ev2idx = {ev:i for i, ev in enumerate(self.code2name.keys())}
+
+    def sort_nts(self, nt_list, codekey):
+        """Sort list of namedtuples such so evidence codes in same order as code2name."""
+        sortby = lambda nt: self.ev2idx.get(getattr(nt, codekey))
+        return sorted(nt_list, key=sortby)
 
     def get_grp_name(self, code):
         """Return group and name for an evidence code."""
@@ -56,5 +64,9 @@ class EvidenceCodes(object):
             grp, name = self.get_grp_name(key.replace("NOT ", ""))
             prt.write("{CNT:7,} {EV:>7} {GROUP:<13} {NAME}\n".format(
                 CNT=cnt, EV=key, GROUP=grp, NAME=name))
+
+    def get_order(self, codes):
+        """Return evidence codes in order shown in cod2name."""
+        return sorted(codes, key=lambda e: [self.ev2idx.get(e)])
 
 # Copyright (C) 2016, DV Klopfenstein, H Tang. All rights reserved."
