a7683db7f9d0857d6cd078884cc78b9bcfd34e92
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2016-10-09 05:19:08 -0400

    Added more checks to test.

--- tests/test_annotations_gaf.py
+++ tests/test_annotations_gaf.py
@@ -8,8 +8,6 @@ __author__ = "DV Klopfenstein"
 
 import os
 import sys
-import wget
-import gzip
 from collections import defaultdict
 from goatools.associations import read_gaf
 from goatools.base import dnld_gafs
@@ -20,21 +18,35 @@ def test_gaf_read(log=sys.stdout):
     species_ids = ['goa_human', 'mgi', 'fb']
     # (optional) multi-level dictionary separate associations by taxid
     taxid2asscs = defaultdict(lambda: defaultdict(lambda: defaultdict(set)))
-    fin_gafs = dnld_gafs(species_ids)
-    for fin_gaf in fin_gafs:
+    local_dir = os.path.dirname(os.path.abspath(__file__))
+    for fin_gaf in dnld_gafs(species_ids):
+        fin_gaf = os.path.join(local_dir, fin_gaf)
+        log.write("\n")
         id2gos = read_gaf(fin_gaf, taxid2asscs=taxid2asscs)
+        if "gene_association.mgi" in fin_gaf:
+            _chk_key(id2gos, "MGI:")
         log.write("  {N:>6,} IDs found in {F}\n".format(N=len(id2gos), F=fin_gaf))
         go2ids = read_gaf(fin_gaf, go2geneids=True)
+        _chk_key(go2ids, "GO:")
         log.write("  {N:>6,} GOs found in {F}\n".format(N=len(go2ids), F=fin_gaf))
     # Report findings stored in optional taxid dictionary
+    log.write("\n")
     for taxid, asscs in taxid2asscs.items():
-        num_gene2gos = len(asscs['ID2GOs'])
-        num_go2genes = len(asscs['GO2IDs'])
+        num_gene2gos = len(asscs.get('ID2GOs'))
+        num_go2genes = len(asscs.get('GO2IDs'))
         log.write("{N:>6,} GOs and {M:>6,} annotated gene ids for tax_id: {TAXID:>6}\n".format(
             TAXID=taxid, N=num_go2genes, M=num_gene2gos))
         # Basic check to ensure gene2go was downloaded and data was returned.
         assert num_gene2gos > 11000
         assert num_go2genes > 6000
+
+def _chk_key(a2bs, pattern):
+    """Confirm format of dictionary key."""
+    for key in a2bs.keys():
+        if pattern in key:
+            return
+        raise RuntimeError("PATTERN({P}) NOT FOUND IN KEY({K})".format(
+            P=pattern, K=key))
 
 if __name__ == '__main__':
     test_gaf_read()
