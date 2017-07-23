1574f3e7c0b09a28f3cc381e21184beac827ff62
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2017-07-23 12:28:00 -0400

    Update tests to use small test go-dag.

--- tests/similarity_top_parent.py
+++ tests/similarity_top_parent.py
@@ -23,41 +23,61 @@ from goatools.semantic import resnik_sim, lin_sim
 def test_top_parent(prt):
     """Semantic Similarity test for Issue #86."""
     fin_obo = "data/i86.obo"
-    fin_obo = "go-basic.obo"
+    branch_dist = 5
     repo = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
     godag = obo_parser.GODag(os.path.join(repo, fin_obo))
     # Get all the annotations from arabidopsis.
-    
+
     # Calculate the semantic distance and semantic similarity:
     _test_path_same(godag, prt)
     _test_path_parallel(godag, prt)
-    #_test_path_bp_mf(godag, prt)
+    _test_path_bp_mf(branch_dist, godag, prt)
     sys.stdout.write("TESTS PASSed: similarity_top_parent\n")
 
-def _test_path_bp_mf(godag, prt):
+def _test_path_bp_mf(branch_dist, godag, prt):
     """Test distances between BP branch and MF branch."""
-    goid_mf = 'GO:0003676' # level-03 depth-03 nucleic acid binding [molecular_function]
-    goid_bp = 'GO:0007516' # level-04 depth-05 hemocyte development [biological_process]
-    sim = semantic_similarity(goid_mf, goid_bp, godag)
-    assert sim is None
-    fin_assc = "http://geneontology.org/gene-associations/gene_association.tair.gz"
-    assc = read_gaf(fin_assc)
+    go_mf = 'GO:0003676' # level-03 depth-03 nucleic acid binding [molecular_function]
+    go_bp = 'GO:0007516' # level-04 depth-05 hemocyte development [biological_process]
+    dst_none = semantic_distance(go_mf, go_bp, godag)
+    sim_none = semantic_similarity(go_mf, go_bp, godag)
+    assc = _get_assc(godag)
     termcounts = TermCounts(godag, assc)
-    sim_r = resnik_sim(goid_mf, goid_bp, godag, termcounts)
-    print('Resnik similarity score ({GO1}, {GO2}) = {VAL}'.format(GO1=goid_mf, GO2=goid_bp, VAL=sim_r))
-    sim_l = lin_sim(goid_mf, goid_bp, godag, termcounts)
-    print('Lin similarity score ({GO1}, {GO2}) = {VAL}'.format(GO1=goid_mf, GO2=goid_bp, VAL=sim_l))
+    fmt = '({GO1}, {GO2}) {TYPE:6} score = {VAL}\n'
+    sim_r = resnik_sim(go_mf, go_bp, godag, termcounts)
+    sim_l = lin_sim(go_mf, go_bp, godag, termcounts)
+    if prt is not None:
+         prt.write(fmt.format(TYPE='semantic distance', GO1=go_mf, GO2=go_bp, VAL=dst_none))
+         prt.write(fmt.format(TYPE='semantic similarity', GO1=go_mf, GO2=go_bp, VAL=sim_none))
+         prt.write(fmt.format(TYPE='Resnik similarity', GO1=go_mf, GO2=go_bp, VAL=sim_r))
+         prt.write(fmt.format(TYPE='Lin similarity', GO1=go_mf, GO2=go_bp, VAL=sim_l))
+    assert dst_none is None
+    assert sim_none is None
+    assert sim_r is None
+    assert sim_l is None
+    sim_d = semantic_distance(go_mf, go_bp, godag, branch_dist)
+    if prt is not None:
+         prt.write(fmt.format(TYPE='semantic distance', GO1=go_mf, GO2=go_bp, VAL=sim_d))
+    assert sim_d == godag[go_mf].depth + godag[go_bp].depth + branch_dist
+
+def _get_assc(godag):
+    """Get association reduced for the test subset of the GO DAG."""
+    fin_assc = "http://geneontology.org/gene-associations/gene_association.tair.gz"
+    assc = {}
+    goids_dag = set(godag.keys())
+    for gene, goids_cur in read_gaf(fin_assc).items():
+        assc[gene] = goids_cur.intersection(goids_dag)
+    return assc
 
 def _test_path_parallel(godag, prt):
     """Test distances between GO IDs on parallel branches."""
-    goid_bottom = 'GO:0007516' # level-04 depth-05 hemocyte development [biological_process]
+    goid_bottom = 'GO:0007516' # BP level-04 depth-05 hemocyte development
     # Test distances up a parallel branch
     goids = [
-        'GO:0044763',  # level-02 depth-02 single-organism cellular process [biological_process]
-        'GO:0008219',  # level-03 depth-03 cell death [biological_process]
-        'GO:0070997',  # level-04 depth-04 neuron death [biological_process]
-        'GO:0036475',  # level-05 depth-05 neuron death in response to oxidative stress [biological_process]
-        'GO:0036476']  # level-06 depth-06 neuron death in response to hydrogen peroxide [biological_process]
+        'GO:0044763',  # BP level-02 depth-02 single-organism cellular process
+        'GO:0008219',  # BP level-03 depth-03 cell death
+        'GO:0070997',  # BP level-04 depth-04 neuron death
+        'GO:0036475',  # BP level-05 depth-05 neuron death in response to oxidative stress
+        'GO:0036476']  # BP level-06 depth-06 neuron death in response to hydrogen peroxide
     fmt = '{DST} semantic_distance between {GO1} and {GO2} on parallel branches\n'
     for dst_exp, goid in enumerate(goids, 3):
         dst_act = semantic_distance(goid_bottom, goid, godag)
@@ -76,7 +96,6 @@ def _test_path_same(godag, prt):
         'GO:0044763', # level-02 depth-02 single-organism cellular process [biological_process]
         'GO:0048869', # level-03 depth-03 cellular developmental process [biological_process]
         'GO:0048468'] # level-03 depth-04 cell development [biological_process]
-    num_goids = len(goids_bp)
     fmt = '{DST} semantic_distance for {GO1} and {GO2} on the same branch\n'
     for dst_exp, goid in enumerate(reversed(goids_bp), 1):
         dst_act = semantic_distance(goid_bottom, goid, godag)
@@ -87,4 +106,3 @@ def _test_path_same(godag, prt):
 if __name__ == '__main__':
     PRT = None if len(sys.argv) != 1 else sys.stdout
     test_top_parent(PRT)
-
