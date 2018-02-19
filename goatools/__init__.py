c315b24b6a35c38092a73466b4c81448f3f973af
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2018-02-19 09:52:29 -0500

    remove: from goatools.go_enrichment import *

--- goatools/__init__.py
+++ goatools/__init__.py
@@ -1,9 +1,9 @@
 from __future__ import absolute_import
 
-# make the module importable
-from goatools.go_enrichment import *
-from . import multiple_testing
-from . import obo_parser
+#### # make the module mportable
+#### from goatools.go_enrichment import *
+#### from . import multiple_testing
+#### from . import obo_parser
 
 __author__ = ("Haibao Tang", "DV Klopfenstein")
 __copyright__ = "Copyright (C) 2009-2018, Haibao Tang, DV Klopfenstein"
