7522638a87102ec966db145d22efaff5feae125e
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2018-02-25 13:20:02 -0500

    GO DAG general information

---
+++ goatools/godag/consts.py
@@ -0,0 +1,15 @@
+"""GO-DAG items."""
+
+__copyright__ = "Copyright (C) 2010-2018, DV Klopfenstein, H Tang, All rights reserved."
+__author__ = "DV Klopfenstein"
+
+
+NAMESPACE2NS = {
+    'biological_process' : 'BP',
+    'molecular_function' : 'MF',
+    'cellular_component' : 'CC'}
+
+RELATIONSHIPS = set(['part_of', 'regulates', 'negatively_regulates', 'positively_regulates'])
+
+
+# Copyright (C) 2010-2018, DV Klopfenstein, H Tang, All rights reserved.
