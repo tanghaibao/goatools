8d1cd7c6eaf02f8fb88ce70a3c5a2a3a68b47f65
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2018-04-02 16:53:48 -0400

    Updated test for latest DAVID download.

--- tests/test_david_nts.py
+++ tests/test_david_nts.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 """Test working with DAVID results in DAVID chart files."""
 
+from __future__ import print_function
+
 __copyright__ = "Copyright (C) 2016-2017, DV Klopfenstein, H Tang, All rights reserved."
 __author__ = "DV Klopfenstein"
 
@@ -18,12 +20,12 @@ def test_david_chart():
     ntobj = cx.namedtuple("david6p8", "TOTAL FDR Bonferroni Benjamini PValue")
     # pylint: disable=bad-whitespace
     fin2exp = {
-        "gjoneska2015_chart6p8_Consistent_Decrease.txt": ntobj._make([  297,  24,  25,  48, 209]),
-        "gjoneska2015_chart6p8_Transient_Decrease.txt":  ntobj._make([   79,   0,   2,   2,  53]),
-        "gjoneska2015_chart6p8_Consistent_Increase.txt": ntobj._make([  418,  33,  33,  76, 307]),
-        "gjoneska2015_chart6p8_Transient_Increase.txt":  ntobj._make([  473, 125, 129, 233, 384]),
-        "gjoneska2015_chart6p8_Late_Decrease.txt":       ntobj._make([  595, 110, 106, 215, 462]),
-        "gjoneska2015_chart6p8_Late_Increase.txt":       ntobj._make([  804,  81,  81, 168, 613]),
+        "gjoneska2015_chart6p8_Consistent_Decrease.txt": ntobj._make([ 1773, 259, 249,  432, 1316]),
+        "gjoneska2015_chart6p8_Transient_Decrease.txt":  ntobj._make([  423,   0,   2,    2,  246]),
+        "gjoneska2015_chart6p8_Consistent_Increase.txt": ntobj._make([ 2359, 353, 308,  781, 1868]),
+        "gjoneska2015_chart6p8_Transient_Increase.txt":  ntobj._make([ 2191, 658, 652, 1105, 1786]),
+        "gjoneska2015_chart6p8_Late_Decrease.txt":       ntobj._make([ 2752, 591, 568, 1153, 2187]),
+        "gjoneska2015_chart6p8_Late_Increase.txt":       ntobj._make([ 4597, 708, 616, 1715, 3603]),
     }
     tic = timeit.default_timer()
     fin2obj = {f:DavidChartReader(os.path.join(david_dir, f)) for f in fin2exp.keys()}
@@ -34,7 +36,8 @@ def test_david_chart():
         obj.prt_num_sig()
         ctr = obj.get_num_sig()
         for fld, cnt_actual in ctr.most_common():
-            assert cnt_actual == getattr(ntexp, fld)
+            assert cnt_actual == getattr(ntexp, fld), "{FIN}: {FLD} Act({ACT}) Exp({EXP})".format(
+                FIN=fin, FLD=fld, ACT=cnt_actual, EXP=getattr(ntexp, fld))
 
 
 if __name__ == '__main__':
