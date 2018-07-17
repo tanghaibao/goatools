e539a1d3a2c2c7913ba65724084fc0f6d003c2f4
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2018-07-17 11:59:48 -0400

    Add object w/all args & dflt vals for tests.

---
+++ goatools/test_data/cli/find_enrichment_dflts.py
@@ -0,0 +1,41 @@
+"""Defaults for find_enrichment parseargs to be used in tests."""
+
+__copyright__ = "Copyright (C) 2016-2018, DV Klopfenstein, H Tang, All rights reserved."
+__author__ = "DV Klopfenstein"
+
+import os
+import collections as cx
+
+
+# pylint: disable=too-few-public-methods
+class ArgsDict(object):
+    """Defaults for find_enrichment parseargs to be used in tests."""
+
+    repo = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../..")
+
+    def __init__(self):
+        self.namespace = {
+            'alpha' : 0.05,
+            'compare' : False,
+            'filenames' : [
+                '{REPO}/data/study'.format(REPO=self.repo),
+                '{REPO}/data/population'.format(REPO=self.repo),
+                '{REPO}/data/association'.format(REPO=self.repo)],
+            'goslim' : '{REPO}/goslim_generic.obo'.format(REPO=self.repo),
+            'indent' : False,
+            'method' : 'bonferroni,sidak,holm,fdr_bh',
+            'min_overlap' : 0.7,
+            'no_propagate_counts' : False,
+            'obo' : '{REPO}/go-basic.obo'.format(REPO=self.repo),
+            'outfile' : '{REPO}/goea.txt'.format(REPO=self.repo),
+            'outfile_detail' : None,
+            'pval' : 0.05,
+            'pval_field' : 'uncorrected',
+            'pvalcalc' : 'fisher',
+            'ratio' : None,
+            'sections' : None,
+        }
+        self.ntobj = cx.namedtuple("Namespace", " ".join(self.namespace.keys()))
+
+
+# Copyright (C) 2016-2018, DV Klopfenstein, H Tang, All rights reserved.
