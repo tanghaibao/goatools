4834053ac2066f9421e48b04425109a0bf8cda29
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2019-07-23 18:51:18 -0400

    Moved chk_relationships from cli.gosubdag_plot to common area

--- goatools/godag/consts.py
+++ goatools/godag/consts.py
@@ -13,11 +13,16 @@ NAMESPACE2NS = {
 RELATIONSHIP_LIST = ['part_of', 'regulates', 'negatively_regulates', 'positively_regulates']
 RELATIONSHIP_SET = set(RELATIONSHIP_LIST)
 
-top_terms = set(['GO:0008150', 'GO:0003674', 'GO:0005575']) # BP, MF, CC
+TOP_TERMS = set(['GO:0008150', 'GO:0003674', 'GO:0005575']) # BP, MF, CC
 NS2GO = {'BP':'GO:0008150', 'MF':'GO:0003674', 'CC':'GO:0005575'}
 NAMESPACE2GO = {'biological_process':'GO:0008150',
                 'molecular_function':'GO:0003674',
                 'cellular_component':'GO:0005575'}
+
+def chk_relationships(relationships):
+    """Check if user-provided relationships are seen in the GO DAG"""
+    assert relationships.issubset(RELATIONSHIP_SET), 'RELATIONSHIP({R}) NOT IN: {Rs}'.format(
+        R=relationships.difference(RELATIONSHIP_SET), Rs=RELATIONSHIP_SET)
 
 
 # Copyright (C) 2010-2019, DV Klopfenstein, H Tang, All rights reserved.
