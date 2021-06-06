d2ee8646dfbd5a344cb2174bc5269cfa5b968030
Haibao Tang <tanghaibao@gmail.com>
2021-06-05 23:27:51 -0700

    Remove unused import

--- tests/test_study_zero.py
+++ tests/test_study_zero.py
@@ -6,7 +6,7 @@ import numpy as np
 
 # from goatools.rpt.goea_nt_xfrm import MgrNtGOEAs  # get_goea_nts_all
 from goatools.test_data.genes_NCBI_10090_ProteinCoding import GENEID2NT as GeneID2nt_mus
-from goatools.test_data.nature3102_goea import get_geneid2symbol, get_goeaobj
+from goatools.test_data.nature3102_goea import get_goeaobj
 from goatools.godag.prtfncs import GoeaPrintFunctions
 
 __copyright__ = "Copyright (C) 2016-2019, DV Klopfenstein, H Tang, All rights reserved."
@@ -28,9 +28,7 @@ def test_example():
     # No study genes at all
     geneids_study_none = set()
     goea_results_all = goeaobj.run_study(geneids_study_none)
-    assert not goea_results_all, "NO STUDY GENES TEST FAILED: {R}".format(
-        R=goea_results_all
-    )
+    assert not goea_results_all, f"NO STUDY GENES TEST FAILED: {goea_results_all}"
     # issue #214: study (geneIDs) cannot be a numpy array
     geneids_study_numpy_array = np.array(["AAA", "BBBBB", "C"])
     goea_results_all = goeaobj.run_study(geneids_study_numpy_array)
@@ -39,9 +37,7 @@ def test_example():
     geneids_study_bad = set(["BADVAL"])
     goea_results_all = goeaobj.run_study(geneids_study_bad)
     # goea_results_sig = [r for r in goea_results_all if r.p_fdr_bh < 0.05]
-    assert not goea_results_all, "NO VALID STUDY GENES TEST FAILED: {R}".format(
-        R=goea_results_all
-    )
+    assert not goea_results_all, f"NO VALID STUDY GENES TEST FAILED: {goea_results_all}"
     # goea_results_all = goeaobj.run_study(geneids_study)
     objprtres = GoeaPrintFunctions()
     objprtres.print_results(goea_results_all, pval=None)
