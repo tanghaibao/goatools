cfe27697b013eb9d45f3f710db09b735a5b79154
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2021-10-26 14:32:24 -0400

    Do an enrichment analysis using Human Phenotype Ontology rather than Gene Ontology https://github.com/tanghaibao/goatools/issues/202

--- tests/test_i202_hpoea.py
+++ tests/test_i202_hpoea.py
@@ -3,13 +3,13 @@
 
 from __future__ import print_function
 
-import os
+#### import os
 from os.path import join
 from tests.utils import REPO
 
-from goatools.base import get_godag
-from goatools.associations import dnld_ncbi_gene_file
-from goatools.associations import read_ncbi_gene2go
+from goatools.obo_parser import GODag
+from goatools.anno.idtogos_reader import IdToGosReader
+#### from goatools.associations import read_ncbi_gene2go
 from goatools.go_enrichment import GOEnrichmentStudy
 from goatools.utils import read_geneset
 
@@ -19,53 +19,38 @@ def test_i202():
     fin_study = 'tests/data/i202_HPO_obo/genes.list'
     fin_pop = 'tests/data/i202_HPO_obo/gobackground.list'
     fin_obo = 'tests/data/i202_HPO_obo/hp.obo'
+    fin_anno = 'tests/data/i202_HPO_obo/hpo.annotation.tab'
 
     study_ids = read_geneset(join(REPO, fin_study))
     population_ids = read_geneset(join(REPO, fin_pop))
-    dag = GODag(
-    return
+    godag = GODag(join(REPO, fin_obo))
+    annoobj = IdToGosReader(join(REPO, fin_anno), goobj=godag)
 
-    obj = _Run(9606, 'gene2go', 'go-basic.obo')
+    ## obj = _Run(9606, 'gene2go', 'go-basic.obo')
 
     # Result is the same whether fisher_scipy_stats of fisher
     pvalcalc = 'fisher_scipy_stats'
-    goeaobj = GOEnrichmentStudy(population_ids, obj.gene2go, obj.godag, methods=['bonferroni', 'fdr_bh'], pvalcalc=pvalcalc)
+    goeaobj = GOEnrichmentStudy(
+        population_ids,
+        annoobj.get_id2gos(),
+        godag,
+        methods=['bonferroni', 'fdr_bh'],
+        pvalcalc=pvalcalc)
     # Run GOEA Gene Ontology Enrichment Analysis
     results_goeas = goeaobj.run_study_nts(study_ids)
     print('NS GO         p stu_ratio pop_ratio    p-uncorr bonferro fdr_bh   stu  ')
     for ntd in results_goeas:
         if ntd.study_count == 0:
-            doprt = False
+            ## doprt = False
             if ntd.p_bonferroni < 0.05:
                 assert ntd.enrichment == 'p'
-                doprt = True
+                ## doprt = True
             if ntd.p_fdr_bh < 0.05:
                 assert ntd.enrichment == 'p'
-                doprt = True
-            if doprt:
-                print(obj.str_nt(ntd))
-    # print(next(iter(results_goeas))._fields)
-
-class _Run():
-    """Run test."""
-
-    patrec = '{NS} {GO} {e} {RS} {RP:>12} {PVAL:8.2e} {BONF:8.2e} {BH:8.2e} {STU}'
-
-    def __init__(self, taxid, fin_gene2go, fin_gobasic):
-        _fin = join(REPO, fin_gene2go)
-        dnld_ncbi_gene_file(_fin, loading_bar=None)
-        self.gene2go = read_ncbi_gene2go(_fin, [taxid])
-
-        _fin_obo = join(REPO, fin_gobasic)
-        self.godag = get_godag(_fin_obo, loading_bar=None)
-
-    def str_nt(self, ntd):
-        return self.patrec.format(
-            NS=ntd.NS, GO=ntd.GO,
-            RS=ntd.ratio_in_study, RP=str(ntd.ratio_in_pop),
-            e=ntd.enrichment,
-            PVAL=ntd.p_uncorrected, BONF=ntd.p_bonferroni, BH=ntd.p_fdr_bh,
-            STU=ntd.study_items)
+                ## doprt = True
+            ## if doprt:
+            ##     print(obj.str_nt(ntd))
+    print(next(iter(results_goeas))._fields)
 
 
 if __name__ == '__main__':
