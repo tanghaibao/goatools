a80acb27dc01d0feb90cd2372134b83329629675
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2023-01-04 11:46:28 -0500

    Begin test for issue 256: How to get most recent common ancesters and shortest path distance between two terms? https://github.com/tanghaibao/goatools/issues/256

--- tests/i148_semsim_lin.py
+++ tests/i148_semsim_lin.py
@@ -2,9 +2,13 @@
 """Test for issue 148, Lin Similarity if a term has no annotations"""
 
 
-import os
-import sys
+from os.path import join
+from os.path import dirname
+from os.path import abspath
+from sys import stdout
+
 from itertools import combinations_with_replacement as combo_w_rplc
+
 from goatools.base import get_godag
 from goatools.associations import dnld_annofile
 from goatools.anno.gpad_reader import GpadReader
@@ -12,20 +16,21 @@ from goatools.anno.gpad_reader import GpadReader
 from goatools.semantic import TermCounts
 #### from goatools.semantic import get_info_content
 #### from goatools.semantic import deepest_common_ancestor
-from goatools.semantic import resnik_sim
+#### from goatools.semantic import resnik_sim
 from goatools.semantic import lin_sim
 #### from goatools.godag.consts import NS2GO
 
-REPO = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
+REPO = join(dirname(abspath(__file__)), "..")
 
-def test_i148_semsim_lin(prt=sys.stdout):
+def test_i148_semsim_lin(prt=stdout):
     """Test for issue 148, Lin Similarity if a term has no annotations"""
-    fin_gpad = os.path.join(REPO, 'goa_human.gpad')
+    fin_gpad = join(REPO, 'goa_human.gpad')
     dnld_annofile(fin_gpad, 'gpad')
 
-    godag = get_godag(os.path.join(REPO, "go-basic.obo"), loading_bar=None)
+    godag = get_godag(join(REPO, "go-basic.obo"), loading_bar=None)
     annoobj = GpadReader(fin_gpad, godag=godag)
 
+    # Researcher-provided GO IDs
     goids = [
         'GO:0042581',
         'GO:0101002',
@@ -40,19 +45,20 @@ def test_i148_semsim_lin(prt=sys.stdout):
 
     # Calculate Lin values
     p2v = {frozenset([a, b]): lin_sim(a, b, godag, termcounts) for a, b in combo_w_rplc(goids, 2)}
-    _prt_values(goids, p2v, prt=sys.stdout)
+    _prt_lins_similarity(goids, p2v, prt)
 
 
-def _prt_values(goids, p2v, prt=sys.stdout):
-    """Print values"""
-    prt.write('           {HDR}\n'.format(HDR=' '.join(goids)))
+def _prt_lins_similarity(goids, p2v, prt):
+    """Print Lin's semantic similarity between all researcher provided GO IDs (goids)"""
+    prt.write("\nLin's semantic similarity between researcher-provided GO IDs:\n")
+    prt.write(f'           {" ".join(goids)}\n')
     none = 'None     '
     for go_row in goids:
-        prt.write('{GO} '.format(GO=go_row))
+        prt.write(f'{go_row} ')
         for go_col in goids:
-            val = p2v[frozenset([go_row, go_col])]
-            txt = '{L:<9.6} '.format(L=val) if val is not None else none
-            prt.write('{T:10} '.format(T=txt))
+            lins_val = p2v[frozenset([go_row, go_col])]
+            lins_str = f'{lins_val:<9.6} ' if lins_val is not None else none
+            prt.write(f'{lins_str:10} ')
         prt.write('\n')
 
 
